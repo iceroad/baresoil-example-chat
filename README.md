1963e0a56fa0ab41333cd56477ddb9253fbb2a19
Mayank Lahiri <mayank@iceroad.io>
2017-05-21 14:16:15 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # baresoil-example-chat
-Baresoil example: web chat
+
+This is a basic web chat demo for the [Baresoil PaaS](https://www.baresoil.com/). See a live demo at [chat.baresoil.cloud](https://chat.baresoil.cloud) (warning: public Internet comments may contain objectional material).
+
+  1. Install the Baresoil CLI: `npm install baresoil -g`
+  2. Clone this repository
+  3. Start the Baresoil Development Environment in the root directory of the project: `baresoil dev`.
+  4. Open the following URL in a web browser: [http://localhost:8086/](http://localhost:8086) to see an autorefreshing preview of the project.
+
+For documentation on Baresoil, see [docs.baresoil.com](https://docs.baresoil.com/).
+
---
+++ server/fn-$session.js
@@ -0,0 +1,40 @@
+module.exports = function(sessionRequest, cb) {
+  var baseConnection = this.baseConnection;
+  var sendEventFn = this.sendEvent.bind(this);
+  var RealtimeBus = this.svclib.RealtimeBus;
+  var KVDataStore = this.svclib.KVDataStore;
+  this.username = (sessionRequest || {}).username;
+
+  //
+  // Subscribe to realtime channel and bounce any received messages back
+  // to the client as user events.
+  //
+  var channelId = 'global-chat';
+
+  RealtimeBus.on('message', function(channelMessage) {
+    sendEventFn('channel_message', channelMessage);
+  });
+
+  RealtimeBus.listen([{
+    channelId: channelId,
+  }], function(err) {
+    if (err) {
+      console.error('Could not subscribe to realtime channel:', e);
+    }
+    return cb();
+  });
+
+  //
+  // Retrieve chat history.
+  //
+  KVDataStore.get([{
+    table: 'history',
+    key: 'global-chat-history'
+  }], (err, items) => {
+    if (err) {
+      console.error(err);
+    } else {
+      this.sendEvent('chat_history', items[0].value || []);
+    }
+  });
+};
